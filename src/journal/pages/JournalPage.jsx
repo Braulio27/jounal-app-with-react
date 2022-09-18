783799482edd3e2aa044753458ea90963cdfe180
Braulio <braulio@focusgte.com>
2022-09-18 17:36:37 -0600

    Implementación de JournalLayouth

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,7 +1,12 @@
 import { Typography } from "@mui/material";
+import { JournalLayout } from "../layout/JournalLayout";
 
 export const JournalPage = () => {
     return (
-        <Typography variant="h1">JournalPage</Typography>
+        <JournalLayout>
+            <Typography>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus placeat exercitationem porro quasi perspiciatis odio, labore eos ratione, cum voluptas incidunt molestiae sequi autem quae officia accusamus illo itaque culpa distinctio ipsum dolores velit neque rem? Quis doloremque non vitae dolorum nostrum tenetur quo ad harum expedita quae, odio eius!
+            </Typography>
+        </JournalLayout>
     )
 }

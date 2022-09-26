6a540b25e2a5713e19fbf3b68149d8df85e7280e
Braulio <braulio@focusgte.com>
2022-09-26 12:08:16 -0600

    Mostrar el mensaje de error de autenticación

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,10 +1,11 @@
 import { Link as RouterLink } from 'react-router-dom';
-import { Button, Grid, Link, TextField, Typography } from "@mui/material"
+import { Alert, Button, Grid, Link, TextField, Typography } from "@mui/material"
 import { AuthLayout } from "../layout/AuthLayout"
 import { useForm } from '../../hooks';
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { startCreatingUserWithEmailPassword } from '../../store/auth/thunks';
+import { useMemo } from 'react';
 
 const formData = {
     email: '',
@@ -21,6 +22,8 @@ const formValidations = {
 export const RegisterPage = () => {
     const dispatch = useDispatch();
     const [formSubmitted, setFormSubmitted] = useState(false);
+    const { status, errorMessage } = useSelector(state => state.auth);
+    const isCheckingAuthentication = useMemo(() => status === 'checking', [status]);
     const {
         formState, displayName, email, password, onInputChange,
         isFormValid, displayNameValid, emailValid, passwordValid,
@@ -77,8 +80,18 @@ export const RegisterPage = () => {
                         />
                     </Grid>
                     <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+                        <Grid
+                            item
+                            xs={12}
+                            display={!!errorMessage ? '' : 'none'}
+                        >
+                            <Alert severity='error'>
+                                {errorMessage}
+                            </Alert>
+                        </Grid>
                         <Grid item xs={12} >
                             <Button
+                                disabled={isCheckingAuthentication}
                                 type="submit"
                                 variant='contained'
                                 fullWidth>

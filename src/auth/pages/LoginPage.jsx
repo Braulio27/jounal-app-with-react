0b6917547c69cf9cd15d0f40398bed257c2e2822
Braulio <braulio@focusgte.com>
2022-09-26 15:20:17 -0600

    Mensajes de error login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -2,13 +2,13 @@ import { useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link as RouterLink } from 'react-router-dom';
 import { Google } from "@mui/icons-material"
-import { Button, Grid, Link, TextField, Typography } from "@mui/material"
+import { Alert, Button, Grid, Link, TextField, Typography } from "@mui/material"
 import { AuthLayout } from '../layout/AuthLayout';
 import { useForm } from '../../hooks';
 import { checkingAuthentication, startLoginWithEmailPassword, startGoogleSignIn } from '../../store/auth';
 
 export const LoginPage = () => {
-    const { status } = useSelector(state => state.auth);
+    const { status, errorMessage } = useSelector(state => state.auth);
     const dispatch = useDispatch();
     const { email, password, onInputChange, formState } = useForm({
         email: 'braulio@google.com',
@@ -51,6 +51,21 @@ export const LoginPage = () => {
                             fullWidth
                         />
                     </Grid>
+                    <Grid
+                        container
+                        display={!!errorMessage ? '' : 'none'}
+                        sx={{ mt: 1 }}
+                    >
+                        <Grid
+                            item
+                            xs={12}
+
+                        >
+                            <Alert severity='error'>
+                                {errorMessage}
+                            </Alert>
+                        </Grid>
+                    </Grid>
                     <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
                         <Grid item xs={12} sm={6}>
                             <Button
@@ -83,6 +98,6 @@ export const LoginPage = () => {
                 </Grid>
                 {/* <code>{status}</code> */}
             </form>
-        </AuthLayout>
+        </AuthLayout >
     )
 }
